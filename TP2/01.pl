72dbf9996cf0c57edbfa6c32cdf969913b30ba21
joao <up201806779@fe.up.pt>
2020-10-18 23:17:37 +0100

    [TP2] initial commit

---
+++ TP2/01.pl
@@ -0,0 +1,12 @@
+r(a,b).
+r(a,c).
+r(b,a).
+r(a,d).
+s(b,c).
+s(b,d).
+s(c,c).
+s(d,e).
+
+/*
+    r(X, Y), s(Y, Z), \+(r(Y, X)), \+(s(Y, Y)). Solução: [a, d, e]
+*/
--- TP2/04.pl
+++ TP2/04.pl
@@ -1,3 +1,10 @@
+factorial(0,1).
+factorial(N,Valor):-
+    N>0,
+    N1 is N-1,
+    factorial(N1,Valor1),
+    Valor is Valor1*N.
+
 fibonacci(0,1).
 fibonacci(1,1).
 fibonacci(N,Valor):-
