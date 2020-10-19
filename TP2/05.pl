a83d7038b300bf35396eb01f747135c8c1f33675
joao <up201806779@fe.up.pt>
2020-10-19 12:23:06 +0100

    [TP2] finished

--- TP2/05.pl
+++ TP2/05.pl
@@ -1,11 +1,11 @@
 divisivel(X, Y):-
-    0 is X mod Y, !.
+    X mod Y =:= 0, !.
 
 divisivel(X, Y) :- 
     X > Y + 1,
     divisivel(X, Y + 1).
 
-primo(N):-
+e_primo(N):-
     N > 1,
     N2 = 2,
     \+ divisivel(N, N2).
