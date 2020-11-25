53844b1170fe716b3a022a629241388fe83fbdfa
TsarkFC <up201806779@fe.up.pt>
2020-11-25 12:27:45 +0000

    [tests/test1/2018] 6 verifying time

--- tests/test1/2018/06.pl
+++ tests/test1/2018/06.pl
@@ -1,14 +1,24 @@
 %tripDays(+Trip, +Time, -FlightTimes, -Days) %Not checking time diferences
 tripDays(Trip, Time, FlightTimes, Days) :-
-    getFlightTimes(Trip, Time, FlightTimes, Time, Days),
-    verifyFlightTimes(FlightTimes).
+    getFlightTimes(Trip, Time, FlightTimes, Time, 0, Days).
 
-getFlightTimes([H | []], _, [], _, 1).
-getFlightTimes([H1, H2|T], Time, [Ht|Tt], LastTime, Days) :-
+getFlightTimes([_ | []], _, [], _, _, 1).
+getFlightTimes([H1, H2|T], Time, [Ht|Tt], LastTime, LastDuration, Days) :-
     airport(_, Id1, H1),
     airport(_, Id2, H2),
-    flight(Flight1, Id1, Id2, Ht, Duration, _),
-    getFlightTimes([H2|T], Time, Tt, Ht, SubDays),
+    flight(_, Id1, Id2, Ht, Duration, _),
+    timeDifference(LastTime, LastDuration, Ht),
+    getFlightTimes([H2|T], Time, Tt, Ht, Duration, SubDays),
     (
         LastTime > Ht -> Days is SubDays + 1 ; Days = SubDays
+    ).
+
+timeDifference(Time1, Duration, Time2) :-
+    TimeHour1 = Time1 // 100,
+    TimeMinutes1 = TimeHour1 * 60 + Time1 mod 100 + Duration,
+    TimeHour2 = Time2 // 100,
+    TimeMinutes2 = TimeHour2 * 60 + Time2 mod 100,
+    Diff is TimeMinutes2 - TimeMinutes1,
+    (
+        Diff >= 30; Diff < 0
     ).
