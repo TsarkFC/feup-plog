dc1a0c5c49bb51512797cdd4799e9d975d999c05
TsarkFC <up201806779@fe.up.pt>
2020-12-28 18:43:28 +0000

    structure

---
+++ tps/plr/tp1/01.pl
@@ -0,0 +1,17 @@
+:-use_module(library(clpfd)).
+
+magic(Vars) :-
+    Vars=[A1,A2,A3,A4,A5,A6,A7,A8,A9],
+    domain(Vars,1,9),
+    %Soma is (9+1)*3//2, % Aumenta a Eficiência
+    all_distinct(Vars),
+    A1+A2+A3 #= Soma,
+    A4+A5+A6 #= Soma,
+    A7+A8+A9 #= Soma,
+    A1+A4+A7 #= Soma,
+    A2+A5+A8 #= Soma,
+    A3+A6+A9 #= Soma,
+    A1+A5+A9 #= Soma,
+    A3+A5+A7 #= Soma,
+    A1 #< A2, A1 #< A3, A1 #< A4, A2 #< A4, % Eliminar simetrias
+    labeling([],Vars).
--- utils/functor2.pl
+++
@@ -1,6 +0,0 @@
-functor2(Term, F, Arity):-
-    Term=..[F|Args],
-    length(Args, Arity).
-
-:-use_module(library(lists)).
-.
