c20084e7907b8b6504ed88e4708a93e4d3734ac8
TsarkFC <up201806779@fe.up.pt>
2021-01-05 17:20:41 +0000

    [tests/test2] model

--- tests/test2/model/05.pl
+++ tests/test2/model/05.pl
@@ -3,10 +3,27 @@
 
 % previous exercises were solved on 2017 test
 
-% !!INCOMPLETE!!
+% embrulha([100,45,70], [12,50,14,8,10,90,24], S).
 % embrulha(+Rolos,+Presentes,-RolosSelecionados)
 embrulha(Rolos, Presentes, RolosSelecionados) :-
-    length(Presentes, NRolos),
-    length(RolosSelecionados, NRolos),
+    length(Presentes, NPresentes),
+    length(RolosSelecionados, NPresentes),
 
-    domain(RolosSelecionados, 1, NRolos).
+    length(Rolos, NRolos),
+    domain(RolosSelecionados, 1, NRolos),
+
+    getTasks(Presentes, RolosSelecionados, Tasks),
+    getMachines(1, Rolos, Machines),
+    cumulatives(Tasks, Machines, [bound(upper)]),
+    labeling([], RolosSelecionados).
+    
+getTasks([], [], []).
+getTasks([P|PresentsT], [R|RsT], [Task|TasksT]) :-
+    Task = task(0, 1, 1, P, R),
+    getTasks(PresentsT, RsT, TasksT).
+
+getMachines(_N, [], []).
+getMachines(N, [R|RolosT], [M|MachinesT]) :-
+    M = machine(N, R),
+    N1 is N+1,
+    getMachines(N1, RolosT, MachinesT).
